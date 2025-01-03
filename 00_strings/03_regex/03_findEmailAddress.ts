7ff2cd9318800d9ebfce6ebd85389d369698e1ec
athena272 <guilhermera272@gmail.com>
2025-01-03 04:53:49 -0300

    Update 03_findEmailAddress.ts

--- 00_strings/03_regex/03_findEmailAddress.ts
+++ 00_strings/03_regex/03_findEmailAddress.ts
@@ -1,6 +1,6 @@
-function findEmails(string: string) {
+function findEmails(text: string) {
     const regex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b/g;
-    const result = string.match(regex);
+    const result = text.match(regex);
     if (result) {
         return result;
     }
