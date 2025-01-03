db15dd398d9a9f87b07e05e329670783e406e439
athena272 <guilhermera272@gmail.com>
2025-01-03 13:08:18 -0300

    Create 01_removeCPFDigits.js

---
+++ 00_strings/exercises/01_removeCPFDigits.js
@@ -0,0 +1,8 @@
+"use strict";
+function removeCPFDigits(cpf) {
+    const regexRemoveNonDigits = /\D/g;
+    return cpf.replace(regexRemoveNonDigits, "");
+}
+console.log("🚀 ~ removeCPFDigits('87409217293'):", removeCPFDigits("87409217293"));
+console.log("🚀 ~ removeCPFDigits('874092172-93'):", removeCPFDigits("874092172-93"));
+console.log("🚀 ~ removeCPFDigits('874.092.172-93'):", removeCPFDigits("874.092.172-93"));
