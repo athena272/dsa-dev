b4c7a887f36060c31fc2563d9098bfd329570fbe
athena272 <guilhermera272@gmail.com>
2025-01-03 18:03:02 -0300

    removing js

--- 00_strings/03_regex/01_cpf_test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function removeNoDigits(cpf) {
-    const regex = /\D/g;
-    return cpf.replace(regex, "");
-}
-console.log("🚀 ~ removeNoDigits(\"04023784799\"):", removeNoDigits("04023784799"));
-console.log("🚀 ~ removeNoDigits(\"213.445.034-82\"):", removeNoDigits("213.445.034-82!"));
--- 00_strings/03_regex/02_endWithBR.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function validateBrDomain(domain) {
-    const regex = /\.br$/;
-    return regex.test(domain);
-}
-console.log("🚀 ~ validateBrDomain(\"batata.com.br\"):", validateBrDomain("batata.com.br"));
-console.log("🚀 ~ validateBrDomain(\"banana.com\"):", validateBrDomain("banana.com"));
