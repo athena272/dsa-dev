36f5690686e40c63fe31cfc0c556c3caef82fba9
athena272 <guilhermera272@gmail.com>
2025-01-03 04:33:57 -0300

    Create 01_cpf_test.js

---
+++ 00_strings/03_regex/01_cpf_test.js
@@ -0,0 +1,7 @@
+function removeNoDigits(cpf) {
+    const regex = /\D/g
+    return cpf.replace(regex, "")
+}
+
+console.log("🚀 ~ removeNoDigits(\"04023784799\"):", removeNoDigits("04023784799"))
+console.log("🚀 ~ removeNoDigits(\"213.445.034-82\"):", removeNoDigits("213.445.034-82!"))
