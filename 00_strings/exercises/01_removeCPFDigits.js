c4c6f13fafb40aed65bd3e095cf9aad687bcddae
athena272 <guilhermera272@gmail.com>
2025-01-03 13:42:45 -0300

    Update 01_removeCPFDigits.js

--- 00_strings/exercises/01_removeCPFDigits.js
+++ 00_strings/exercises/01_removeCPFDigits.js
@@ -1,4 +1,13 @@
 "use strict";
+function alternativeVersion(cpf) {
+    let onlyCpfNumbers = '';
+    for (let index = 0; index < cpf.length; index++) {
+        if (cpf[index] >= '0' && cpf[index] <= '9') {
+            onlyCpfNumbers += cpf[index];
+        }
+    }
+    return onlyCpfNumbers;
+}
 function removeCPFDigits(cpf) {
     const regexRemoveNonDigits = /\D/g;
     return cpf.replace(regexRemoveNonDigits, "");
@@ -6,3 +15,7 @@ function removeCPFDigits(cpf) {
 console.log("🚀 ~ removeCPFDigits('87409217293'):", removeCPFDigits("87409217293"));
 console.log("🚀 ~ removeCPFDigits('874092172-93'):", removeCPFDigits("874092172-93"));
 console.log("🚀 ~ removeCPFDigits('874.092.172-93'):", removeCPFDigits("874.092.172-93"));
+console.log("🚀 ~ ------------------------------------------------------------------------ 🚀 ~");
+console.log("🚀 ~ alternativeVersion('87409217293'):", alternativeVersion("87409217293"));
+console.log("🚀 ~ alternativeVersion('874092172-93'):", alternativeVersion("874092172-93"));
+console.log("🚀 ~ alternativeVersion('874.092.172-93'):", alternativeVersion("874.092.172-93"));
