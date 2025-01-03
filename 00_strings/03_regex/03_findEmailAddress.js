480bfca5f7b422b201a67fef135eecb8e04b7087
athena272 <guilhermera272@gmail.com>
2025-01-03 04:53:11 -0300

    Create 03_findEmailAddress.js

---
+++ 00_strings/03_regex/03_findEmailAddress.js
@@ -0,0 +1,21 @@
+"use strict";
+function findEmails(string) {
+    const regex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b/g;
+    const result = string.match(regex);
+    if (result) {
+        return result;
+    }
+    return [];
+}
+let text = "Para mais informações, contate-nos em contato@exemplo.com ou suporte@exemplo.com.br.";
+let emails = findEmails(text);
+console.log("🚀 ~ emails:", emails);
+if (emails.length > 0) {
+    console.log("Emails encontrados:");
+    for (let i = 0; i < emails.length; i++) {
+        console.log(emails[i]);
+    }
+}
+else {
+    console.log("Nenhum email encontrado.");
+}
