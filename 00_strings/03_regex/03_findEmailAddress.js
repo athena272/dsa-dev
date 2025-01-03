7670ad9b87bee359959f6a55d2d453e12b5b996d
athena272 <guilhermera272@gmail.com>
2025-01-03 04:53:52 -0300

    Update 03_findEmailAddress.js

--- 00_strings/03_regex/03_findEmailAddress.js
+++ 00_strings/03_regex/03_findEmailAddress.js
@@ -1,7 +1,7 @@
 "use strict";
-function findEmails(string) {
+function findEmails(text) {
     const regex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b/g;
-    const result = string.match(regex);
+    const result = text.match(regex);
     if (result) {
         return result;
     }
