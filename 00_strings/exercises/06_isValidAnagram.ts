8cf4424d347b4a5b3863c181622773866047ceab
athena272 <guilhermera272@gmail.com>
2025-01-05 02:02:22 -0300

    Update 06_isValidAnagram.ts

--- 00_strings/exercises/06_isValidAnagram.ts
+++ 00_strings/exercises/06_isValidAnagram.ts
@@ -4,18 +4,36 @@ type IsAnagramArgs = {
 }
 
 
-function isAnagram({ text1, text2 }: IsAnagramArgs) {
+function isAnagram1({ text1, text2 }: IsAnagramArgs) {
     const text1Sorted = text1.split('').sort().join('')
-    console.log("🚀 ~ isAnagram ~ text1Sorted:", text1Sorted)
     const text2Sorted = text2.split('').sort().join('')
-    console.log("🚀 ~ isAnagram ~ text2Sorted:", text2Sorted)
-
     return text1Sorted === text2Sorted
 }
 
+function isAnagram({ text1, text2 }: IsAnagramArgs) {
+    const arrayZeros: number[] = new Array(26).fill(0)
 
-console.log("🚀 ~ isAnagram({text1: \"anagram\", text2: \"nagaram\"}):", isAnagram({text1: "anagram", text2: "nagaram"}))
+    for (let i = 0; i < text1.length; i++) {
+        const index = text1.charCodeAt(i) - 'a'.charCodeAt(0)
+        arrayZeros[index]++
+    }
+
+    for (let i = 0; i < text2.length; i++) {
+        const index = text2.charCodeAt(i) - 'a'.charCodeAt(0)
+        arrayZeros[index]--
+    }
+
+    for (let value of arrayZeros) {
+        if (value !== 0) {
+            return false
+        }
+    }
+
+    return true
+}
+
+console.log("🚀 ~ isAnagram({text1: \"anagram\", text2: \"nagaram\"}):", isAnagram({ text1: "anagram", text2: "nagaram" }))
 console.log("🚀 ~ ------------------------------------------------------------------------ 🚀 ~")
-console.log("🚀 ~ isAnagram({text1: \"rat\", text2: \"car\"}):", isAnagram({text1: "rat", text2: "car"}))
+console.log("🚀 ~ isAnagram({text1: \"rat\", text2: \"car\"}):", isAnagram({ text1: "rat", text2: "car" }))
 console.log("🚀 ~ ------------------------------------------------------------------------ 🚀 ~")
-console.log("🚀 ~ isAnagram({text1: \"aaabb\", text2: \"ababab\"}):", isAnagram({text1: "aaabbb", text2: "ababab"}))
+console.log("🚀 ~ isAnagram({text1: \"aaabb\", text2: \"ababab\"}):", isAnagram({ text1: "aaabbb", text2: "ababab" }))
