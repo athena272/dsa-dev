f8259a7a22d85b722f7284ca3f61d45526a6f948
athena272 <guilhermera272@gmail.com>
2025-01-07 06:47:15 -0300

    Update 01_StringMatch-1408.ts

--- dailyChallenge/01_StringMatch-1408.ts
+++ dailyChallenge/01_StringMatch-1408.ts
@@ -3,7 +3,13 @@ function stringMatching(words: string[]) {
 
     for (var i = 0; i < words.length; i++) {
         for (var j = 0; j < words.length; j++) {
-            if (words[i] !== words[j] && words[i].includes(words[j]) && !substringArray.includes(words[j])) {
+            const isSameWord = words[i] === words[j] 
+            if (isSameWord)
+                continue
+
+            const isSubstring = words[i].includes(words[j])
+            const isNotAlreadySubstring = !substringArray.includes(words[j])
+            if (isSubstring && isNotAlreadySubstring) {
                 substringArray.push(words[j]);
             }
         }
