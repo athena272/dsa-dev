4144303a0f03422bb8127e78cf9168aa959a7703
athena272 <guilhermera272@gmail.com>
2025-01-12 17:10:15 -0300

    Update 08_containsDuplicate.ts

--- 01_arrays/exercises/08_containsDuplicate.ts
+++ 01_arrays/exercises/08_containsDuplicate.ts
@@ -1,11 +1,12 @@
 function containsDuplicate(nums: number[]): boolean {
-    for (let i = 0; i < nums.length; i++) {
+    for (let i = 0; i < nums.length - 1; i++) {
         for (let j = i + 1; j < nums.length; j++) {
             if (nums[i] === nums[j]) {
                 return true
             }
         }
     }
+
 
     return false
 }
