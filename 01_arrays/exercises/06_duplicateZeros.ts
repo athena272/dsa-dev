e0b2c6cf0478af939b40aaebbc6fbbd2c85c662d
athena272 <guilhermera272@gmail.com>
2025-01-12 01:00:44 -0300

    Update 06_duplicateZeros.ts

--- 01_arrays/exercises/06_duplicateZeros.ts
+++ 01_arrays/exercises/06_duplicateZeros.ts
@@ -4,7 +4,7 @@ function duplicateZeros(array: number[]) {
             for (let j = array.length - 2; j > i; j--) {
                 array[j + 1] = array[j] //shift right
             }
-            array[i + 1] = array[i]
+            array[i + 1] = 0 // array[i], always will be 0
             i++
         }
     }
