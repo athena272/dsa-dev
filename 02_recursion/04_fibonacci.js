5159422a02bdf038bfe0a8af311886ca4e23de17
athena272 <guilhermera272@gmail.com>
2025-01-18 02:18:12 -0300

    Create 04_fibonacci.js

---
+++ 02_recursion/04_fibonacci.js
@@ -0,0 +1,14 @@
+function fibonacci(num) {
+    if (num === 0)
+        return 0;
+    if (num === 1)
+        return 1;
+    return fibonacci(num - 1) + fibonacci(num - 2);
+}
+console.log("🚀 ~ fibonacci(0):", fibonacci(0));
+console.log("🚀 ~ fibonacci(1):", fibonacci(1));
+console.log("🚀 ~ fibonacci(6):", fibonacci(6));
+console.log("🚀 ~ fibonacci(7):", fibonacci(7));
+console.log("🚀 ~ fibonacci(8):", fibonacci(8));
+console.log("🚀 ~ fibonacci(29):", fibonacci(29));
+console.log("🚀 ~ fibonacci(200):", fibonacci(200));
